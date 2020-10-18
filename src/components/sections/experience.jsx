33b38d58f2d27c0ca39d5073c0815db2b4f0beb1
pedroz2 <pedroz@umich.edu>
2020-10-18 13:58:43 -0400

    Quick commit script.

--- src/components/sections/experience.jsx
+++ src/components/sections/experience.jsx
@@ -7,47 +7,33 @@ class Experience extends Component {
     active: 0,
     experiences: [
       {
-        company: 'Comau',
-        url: 'https://www.comau.com/en',
+        company: 'USHR Inc.',
+        url: 'https://www.ushrauto.com/',
         title: 'Software Engineering Intern',
-        start: 'January 2020',
-        end: 'Present',
+        start: 'May 2020',
+        end: 'October 2020',
         city: 'Ann Arbor',
         state: 'MI',
         details: [
-          'Architect and implement a live bin-packing heuristic in C++ that identifies items’ optimal placement locations, maximizing capacity to 85% and speeding up company automation.',
-          'Create a command-line interface for the robotic system that enables users to visualize item placements and possible future placements step-by-step, powered by Processing.',
+          'Utilized Python to maintain road-mapping software responsible for providing data on over 500,000 miles of road throughout North America used by the autonomous vehicle systems of Nissan, Cadillac, and Rivian. ',
+          'Led initiative to refactor publishing software in anticipation for General Motors’ 2021 contract requirements, including implementation of newly requested road objects, and a 19% improvement of AWS EC2 usage.',
         ],
         color: 'blue',
       },
       {
-        company: 'Capital One',
-        url: 'https://www.capitalone.com/',
+        company: 'The University of Michigan — LSA Technology Services',
+        url: 'https://lsa.umich.edu/technology-services/',
         title: 'Software Engineering Intern',
-        start: 'June 2020',
-        end: 'August 2020',
-        city: 'Chicago',
-        state: 'IL',
+        start: 'March 2019',
+        end: 'Feburary 2020',
+        city: 'Ann Arbor',
+        state: 'MI',
         details: [
           'Developed an AWS Lambda with Python that transforms 3,000+ customer calls each day into visualizable data for Sage, Capital One’s call assessment platform.',
           'Built and integrated experience-elevating features into Sage’s audio player platform with AngularJS and D3.js, including: dual-speaker waveform visualization to distinguish the current speaker and to enhance audio-scrubbing, call captioning to improve audio retention, and issue-tagging to flag any timestamp for future reference.',
           'Interfaced with designers, product managers, and other developers in Agile sprints to ensure functional and thoughtful user experiences across our voice-based applications.',
         ],
         color: 'green',
-      },
-      {
-        company: 'University of Michigan, EECS',
-        url: 'https://eecs.engin.umich.edu/',
-        title: 'Teaching Assistant for EECS 281',
-        start: 'August 2020',
-        end: 'Present',
-        city: 'Ann Arbor',
-        state: 'MI',
-        details: [
-          'Help professors teach 900+ students by instructing lab class, answering online questions, writing and evaluating exams, and holding office hours to guide students one-on-one.',
-          'Deepen students’ understanding of best coding practices, debugging tools, and core concepts, including stacks, queues, trees, dynamic programming, and hash tables.',
-        ],
-        color: 'yellow',
       },
     ],
   };
@@ -73,7 +59,7 @@ class Experience extends Component {
         </p>
         {experiences.map(
           (experience, index) =>
-            active === index && <Role experience={experience} key={index} />
+            <Role experience={experience} key={index} />
         )}
       </Section>
     );
