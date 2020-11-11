a5c079edaa87c6ed8f293ce3013d6307e68acaf3
pedroz2 <pedroz@umich.edu>
2020-11-11 01:22:36 -0500

    fade isn't working on mobile, remove for now

--- src/react/components/section.jsx
+++ src/react/components/section.jsx
@@ -4,14 +4,14 @@ import Fade from 'react-reveal/Fade';
 class Section extends Component {
   render() {
     return (
-      <Fade>
+      //<Fade>
         <div className={`${this.props.title} section`} id={this.props.title}>
           <div className='section-title'>{this.props.title}
             {this.props.title === 'projects' && (<p className='section-project'>Ask me about them!</p>)}
           </div>
           <div className='section-content'>{this.props.children}</div>
         </div>
-      </Fade>
+      //</Fade>
     );
   }
 }
