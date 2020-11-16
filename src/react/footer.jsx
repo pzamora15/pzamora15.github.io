787cbe09b0ffe58458b876401bb97550af2b9c2d
pedroz2 <pedroz@umich.edu>
2020-11-16 12:46:37 -0500

    footer changed to 3 lines

--- src/react/footer.jsx
+++ src/react/footer.jsx
@@ -6,7 +6,8 @@ class Footer extends Component {
     return (
       <div className='footer'>
         <p align='center'> &#169; Pedro Zamora, Oct. 2020</p>
-        <p align='center' style={{fontSize: "0.8em"}}>Built while watching Arrival (2016). Great movie. | Inspired by <a
+        <p align='center' style={{fontSize: "0.8em"}}>Built while watching Arrival (2016). Great movie.</p>
+        <p align='center' style={{fontSize: "0.8em"}}>Inspired by <a
             href='https://shannonlau.com/'
             target='_blank'
             style={{textDecoration: 'none', color: '#6c6b6b'}}
