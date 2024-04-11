ef1ac44229ebaff96f1c41544b8dab189c806d8a
Pedro <pedro@Pedros-Air.home>
2024-04-11 08:59:37 -0700

    Updates

---
+++ precache-manifest.21b37194be76b028c992195c62120685.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13efc6dd935ca2c0fef6317fd9a0e273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6d5c087cea0a65d87ea",
+    "url": "/static/css/main.a2dddaaf.chunk.css"
+  },
+  {
+    "revision": "dff5745b828a9cec4ab8",
+    "url": "/static/js/2.0d799d40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d799d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d5c087cea0a65d87ea",
+    "url": "/static/js/main.1dd621cf.chunk.js"
+  },
+  {
+    "revision": "f99cc12f948d225fb7b6",
+    "url": "/static/js/runtime-main.4938438c.js"
+  },
+  {
+    "revision": "f6753a521455302d80f81ff086486e34",
+    "url": "/static/media/pz.f6753a52.svg"
+  }
+]);
